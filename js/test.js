418535d82da354430027c12588a4617c77ac1594
tarakasaiakhilnandu <tarakasaiakhilnandu@gmail.com>
2026-08-03 08:40:08 +0000

    eventlistners

--- js/test.js
+++ js/test.js
@@ -1,1 +1,16 @@
-console.log("hello world")
+// console.log("hello world")
+// write a function to square the number
+function squareNumber(num){
+    return num * num;
+}
+x=squareNumber(5);
+console.log("Square of 5 is:",x)
+// write a function to odd or even number
+function checkOddEven(num){
+    if (num%2==0) {
+        return "Even";
+     } else{
+        return "Odd";
+     }
+}
+console.log("check if 7 is Odd or Even:", checkOddEven(7));
